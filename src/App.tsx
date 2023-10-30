183e3030af0c5d536894ca590383f3517d3504f9
winters <winters0727@gmail.com>
2023-10-31 00:45:00 +0900

    test : 배포 테스트 메시지 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import { CARDS } from "./constant/card";
+import { flattenCards, groupByValue } from "./utils/card";
+import { createDeckArray, createDeckCode, parseDeckCode } from "./utils/deck";
 
 function App() {
+  const [deck, setDeck] = useState({
+    "ST1-016": 4,
+    "ST2-017": 3,
+    "ST3-017": 2,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{JSON.stringify(deck)}</div>
+      <div>{createDeckCode(deck)}</div>
+      <div>{JSON.stringify(parseDeckCode(createDeckCode(deck)))}</div>
+      {createDeckArray(deck).map((card) => (
+        <img
+          alt={card}
+          src={`https://cookierunbraverse.com/images/card/${
+            card.split("-")[0]
+          }_${card.split("-")[1].slice(1)}.png`}
+        />
+      ))}
     </div>
   );
 }
